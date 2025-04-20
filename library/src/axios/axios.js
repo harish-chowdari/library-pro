924ef20e4a3221acf9e5a09c8232f9046d249711
harish-nagubadi <Harish.N@veots.com>
2025-04-20 17:13:19 +0530

    axios update

--- library/src/axios/axios.js
+++ library/src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:3002" 
+// const BASE_URL = "http://localhost:3002" 
+const BASE_URL = "https://library-pro.onrender.com"
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
